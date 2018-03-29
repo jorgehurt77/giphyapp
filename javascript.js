3c0967fa43873ba3b5fcab995f2c7f5153ea961e
jorgehurt77 <jorgehurt@yahoo.com>
2018-03-29 16:04:44 -0500

    second commit

--- javascript.js
+++ javascript.js
@@ -3,7 +3,7 @@ $(function(){
     console.log("Page Loaded");
 })
 
-var searchArray = ['Dog', 'Cat', 'Bird'];
+var searchArray = ['Dog','Cat','Bird'];
 
 function populateButtons(searchArray,classToAdd,areaToAdd){
     $(areaToAdd).empty();
@@ -18,5 +18,24 @@ function populateButtons(searchArray,classToAdd,areaToAdd){
 
 $(document).on('click','.searchButton',function(){
     var type = $(this).data('type');
-    var queryURL = 'http://api.giphy.com/v1/gifs/search?q='+type+'&api_keyTxGPb5QOCHX5JL2miMqBFXzPbaZVXy9Climit=10'
+    var queryURL = 'http://api.giphy.com/v1/gifs/search?q='+type+'&api_keyTxGPb5QOCHX5JL2miMqBFXzPbaZVXy9Climit=10';
+    $.ajax({url:queryURL,method:'GET'})
+        .done(function(responce){
+            for(var i=0;i<responce.data.lenght;i++){
+                var searchDiv = $('<div class="search=item">');
+                var rating = responce.data[i].rating;
+                var p = $('<p>').text('Rating: '+rating);
+                var animated = responce.data[i].images.fixed_height.url;
+                var still = responce.data[i].images.fixed_height_still.url;
+                var image =$('<img');
+                image.attr('src',still);
+                image.attr('data-still',still);
+                image.attr('data-animated',animated);
+                image.attr('data-state','still');
+                image.addClass('searchImage');
+                searchDiv.append(p);
+                searchDiv.append(image);
+                $('searches').append(searchDiv);
+            }
+    })
 })
